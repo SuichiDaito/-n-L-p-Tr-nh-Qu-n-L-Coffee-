d1d254c29beb7dec7acd491c421ef30e92a50470
AnzGown <135236272+AnzGown@users.noreply.github.com>
2024-05-17 08:35:39 +0700

    link thống kê

--- self-discipline/self-discipline/QuanLy.Designer.cs
+++ self-discipline/self-discipline/QuanLy.Designer.cs
@@ -55,9 +55,9 @@
             | System.Windows.Forms.AnchorStyles.Right)));
             this.panel2.Controls.Add(this.guna2Panel2);
             this.panel2.Location = new System.Drawing.Point(0, 1);
-            this.panel2.Margin = new System.Windows.Forms.Padding(2, 3, 2, 3);
+            this.panel2.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
             this.panel2.Name = "panel2";
-            this.panel2.Size = new System.Drawing.Size(1426, 113);
+            this.panel2.Size = new System.Drawing.Size(1901, 139);
             this.panel2.TabIndex = 3;
             // 
             // guna2Panel2
@@ -68,8 +68,9 @@
             this.guna2Panel2.Dock = System.Windows.Forms.DockStyle.Fill;
             this.guna2Panel2.FillColor = System.Drawing.Color.FromArgb(((int)(((byte)(107)))), ((int)(((byte)(36)))), ((int)(((byte)(12)))));
             this.guna2Panel2.Location = new System.Drawing.Point(0, 0);
+            this.guna2Panel2.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.guna2Panel2.Name = "guna2Panel2";
-            this.guna2Panel2.Size = new System.Drawing.Size(1426, 113);
+            this.guna2Panel2.Size = new System.Drawing.Size(1901, 139);
             this.guna2Panel2.TabIndex = 0;
             // 
             // btnDangXuat
@@ -77,7 +78,7 @@
             this.btnDangXuat.Anchor = System.Windows.Forms.AnchorStyles.Right;
             this.btnDangXuat.AutoRoundedCorners = true;
             this.btnDangXuat.BackColor = System.Drawing.Color.Transparent;
-            this.btnDangXuat.BorderRadius = 21;
+            this.btnDangXuat.BorderRadius = 26;
             this.btnDangXuat.DisabledState.BorderColor = System.Drawing.Color.DarkGray;
             this.btnDangXuat.DisabledState.CustomBorderColor = System.Drawing.Color.DarkGray;
             this.btnDangXuat.DisabledState.FillColor = System.Drawing.Color.FromArgb(((int)(((byte)(169)))), ((int)(((byte)(169)))), ((int)(((byte)(169)))));
@@ -85,9 +86,10 @@
             this.btnDangXuat.FillColor = System.Drawing.Color.FromArgb(((int)(((byte)(245)))), ((int)(((byte)(204)))), ((int)(((byte)(160)))));
             this.btnDangXuat.Font = new System.Drawing.Font("Segoe UI", 11.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.btnDangXuat.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(107)))), ((int)(((byte)(36)))), ((int)(((byte)(12)))));
-            this.btnDangXuat.Location = new System.Drawing.Point(1182, 36);
+            this.btnDangXuat.Location = new System.Drawing.Point(1576, 44);
+            this.btnDangXuat.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.btnDangXuat.Name = "btnDangXuat";
-            this.btnDangXuat.Size = new System.Drawing.Size(180, 45);
+            this.btnDangXuat.Size = new System.Drawing.Size(240, 55);
             this.btnDangXuat.TabIndex = 2;
             this.btnDangXuat.Text = "Đăng xuất";
             this.btnDangXuat.Click += new System.EventHandler(this.btnDangXuat_Click);
@@ -96,9 +98,10 @@
             // 
             this.picLogo.BackgroundImage = global::self_discipline.Properties.Resources.logo;
             this.picLogo.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
-            this.picLogo.Location = new System.Drawing.Point(27, 11);
+            this.picLogo.Location = new System.Drawing.Point(36, 14);
+            this.picLogo.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.picLogo.Name = "picLogo";
-            this.picLogo.Size = new System.Drawing.Size(105, 93);
+            this.picLogo.Size = new System.Drawing.Size(140, 114);
             this.picLogo.TabIndex = 0;
             this.picLogo.TabStop = false;
             // 
@@ -109,9 +112,10 @@
             this.label1.BackColor = System.Drawing.Color.Transparent;
             this.label1.Font = new System.Drawing.Font("MS Reference Sans Serif", 35.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.label1.ForeColor = System.Drawing.Color.Transparent;
-            this.label1.Location = new System.Drawing.Point(352, 22);
+            this.label1.Location = new System.Drawing.Point(469, 27);
+            this.label1.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(712, 59);
+            this.label1.Size = new System.Drawing.Size(882, 73);
             this.label1.TabIndex = 1;
             this.label1.Text = "Cửa hàng The Coffee House";
             this.label1.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
@@ -121,9 +125,10 @@
             this.panBody.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
             | System.Windows.Forms.AnchorStyles.Left) 
             | System.Windows.Forms.AnchorStyles.Right)));
-            this.panBody.Location = new System.Drawing.Point(283, 120);
+            this.panBody.Location = new System.Drawing.Point(377, 148);
+            this.panBody.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.panBody.Name = "panBody";
-            this.panBody.Size = new System.Drawing.Size(1130, 654);
+            this.panBody.Size = new System.Drawing.Size(1507, 805);
             this.panBody.TabIndex = 5;
             // 
             // btnBieuDo
@@ -133,12 +138,14 @@
             this.btnBieuDo.DisabledState.CustomBorderColor = System.Drawing.Color.DarkGray;
             this.btnBieuDo.DisabledState.FillColor = System.Drawing.Color.FromArgb(((int)(((byte)(169)))), ((int)(((byte)(169)))), ((int)(((byte)(169)))));
             this.btnBieuDo.DisabledState.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(141)))), ((int)(((byte)(141)))), ((int)(((byte)(141)))));
+            this.btnBieuDo.Enabled = false;
             this.btnBieuDo.FillColor = System.Drawing.Color.FromArgb(((int)(((byte)(153)))), ((int)(((byte)(77)))), ((int)(((byte)(28)))));
             this.btnBieuDo.Font = new System.Drawing.Font("Segoe UI Black", 15F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.btnBieuDo.ForeColor = System.Drawing.Color.White;
-            this.btnBieuDo.Location = new System.Drawing.Point(-20, 465);
+            this.btnBieuDo.Location = new System.Drawing.Point(-27, 572);
+            this.btnBieuDo.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.btnBieuDo.Name = "btnBieuDo";
-            this.btnBieuDo.Size = new System.Drawing.Size(300, 118);
+            this.btnBieuDo.Size = new System.Drawing.Size(400, 145);
             this.btnBieuDo.TabIndex = 4;
             this.btnBieuDo.Text = "Biểu đồ";
             // 
@@ -152,9 +159,10 @@
             this.btnThongKe.FillColor = System.Drawing.Color.FromArgb(((int)(((byte)(153)))), ((int)(((byte)(77)))), ((int)(((byte)(28)))));
             this.btnThongKe.Font = new System.Drawing.Font("Segoe UI Black", 15F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.btnThongKe.ForeColor = System.Drawing.Color.White;
-            this.btnThongKe.Location = new System.Drawing.Point(-3, 353);
+            this.btnThongKe.Location = new System.Drawing.Point(-4, 434);
+            this.btnThongKe.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.btnThongKe.Name = "btnThongKe";
-            this.btnThongKe.Size = new System.Drawing.Size(280, 106);
+            this.btnThongKe.Size = new System.Drawing.Size(373, 130);
             this.btnThongKe.TabIndex = 3;
             this.btnThongKe.Text = "Thống Kê";
             this.btnThongKe.Click += new System.EventHandler(this.btnThongKe_Click);
@@ -168,9 +176,10 @@
             this.btnQuanLi.FillColor = System.Drawing.Color.FromArgb(((int)(((byte)(153)))), ((int)(((byte)(77)))), ((int)(((byte)(28)))));
             this.btnQuanLi.Font = new System.Drawing.Font("Segoe UI Black", 15F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.btnQuanLi.ForeColor = System.Drawing.Color.White;
-            this.btnQuanLi.Location = new System.Drawing.Point(-3, 238);
+            this.btnQuanLi.Location = new System.Drawing.Point(-4, 293);
+            this.btnQuanLi.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.btnQuanLi.Name = "btnQuanLi";
-            this.btnQuanLi.Size = new System.Drawing.Size(283, 109);
+            this.btnQuanLi.Size = new System.Drawing.Size(377, 134);
             this.btnQuanLi.TabIndex = 2;
             this.btnQuanLi.Text = "Quản lí";
             this.btnQuanLi.Click += new System.EventHandler(this.btnQuanLi_Click);
@@ -184,9 +193,10 @@
             this.btnBanHang.FillColor = System.Drawing.Color.FromArgb(((int)(((byte)(153)))), ((int)(((byte)(77)))), ((int)(((byte)(28)))));
             this.btnBanHang.Font = new System.Drawing.Font("Segoe UI Black", 15F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.btnBanHang.ForeColor = System.Drawing.Color.White;
-            this.btnBanHang.Location = new System.Drawing.Point(-3, 126);
+            this.btnBanHang.Location = new System.Drawing.Point(-4, 155);
+            this.btnBanHang.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.btnBanHang.Name = "btnBanHang";
-            this.btnBanHang.Size = new System.Drawing.Size(280, 106);
+            this.btnBanHang.Size = new System.Drawing.Size(373, 130);
             this.btnBanHang.TabIndex = 1;
             this.btnBanHang.Text = "Bán hàng";
             this.btnBanHang.Click += new System.EventHandler(this.btnBanHang_Click);
@@ -202,9 +212,10 @@
             this.btnThongTin.FillColor = System.Drawing.Color.FromArgb(((int)(((byte)(153)))), ((int)(((byte)(77)))), ((int)(((byte)(28)))));
             this.btnThongTin.Font = new System.Drawing.Font("Segoe UI Black", 15F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.btnThongTin.ForeColor = System.Drawing.Color.White;
-            this.btnThongTin.Location = new System.Drawing.Point(-3, 18);
+            this.btnThongTin.Location = new System.Drawing.Point(-4, 22);
+            this.btnThongTin.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.btnThongTin.Name = "btnThongTin";
-            this.btnThongTin.Size = new System.Drawing.Size(280, 102);
+            this.btnThongTin.Size = new System.Drawing.Size(373, 126);
             this.btnThongTin.TabIndex = 0;
             this.btnThongTin.Text = "Thông tin";
             this.btnThongTin.Click += new System.EventHandler(this.btnThongTin_Click);
@@ -221,9 +232,10 @@
             this.guna2Panel1.Controls.Add(this.btnBanHang);
             this.guna2Panel1.Controls.Add(this.btnQuanLi);
             this.guna2Panel1.FillColor = System.Drawing.Color.FromArgb(((int)(((byte)(107)))), ((int)(((byte)(36)))), ((int)(((byte)(12)))));
-            this.guna2Panel1.Location = new System.Drawing.Point(0, 112);
+            this.guna2Panel1.Location = new System.Drawing.Point(0, 138);
+            this.guna2Panel1.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.guna2Panel1.Name = "guna2Panel1";
-            this.guna2Panel1.Size = new System.Drawing.Size(280, 671);
+            this.guna2Panel1.Size = new System.Drawing.Size(373, 826);
             this.guna2Panel1.TabIndex = 3;
             // 
             // lblTenNVDangNhap
@@ -232,9 +244,10 @@
             this.lblTenNVDangNhap.BackColor = System.Drawing.Color.Transparent;
             this.lblTenNVDangNhap.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.lblTenNVDangNhap.ForeColor = System.Drawing.Color.White;
-            this.lblTenNVDangNhap.Location = new System.Drawing.Point(99, 642);
+            this.lblTenNVDangNhap.Location = new System.Drawing.Point(132, 790);
+            this.lblTenNVDangNhap.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.lblTenNVDangNhap.Name = "lblTenNVDangNhap";
-            this.lblTenNVDangNhap.Size = new System.Drawing.Size(117, 20);
+            this.lblTenNVDangNhap.Size = new System.Drawing.Size(147, 25);
             this.lblTenNVDangNhap.TabIndex = 6;
             this.lblTenNVDangNhap.Text = " Tên thành viên";
             // 
@@ -244,22 +257,24 @@
             this.label2.BackColor = System.Drawing.Color.Transparent;
             this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.label2.ForeColor = System.Drawing.Color.White;
-            this.label2.Location = new System.Drawing.Point(3, 642);
+            this.label2.Location = new System.Drawing.Point(4, 790);
+            this.label2.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(90, 20);
+            this.label2.Size = new System.Drawing.Size(116, 25);
             this.label2.TabIndex = 5;
             this.label2.Text = "Thành viên:";
             // 
             // frmQuanLy
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(1425, 786);
+            this.ClientSize = new System.Drawing.Size(1900, 967);
             this.Controls.Add(this.guna2Panel1);
             this.Controls.Add(this.panBody);
             this.Controls.Add(this.panel2);
             this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
             this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.Name = "frmQuanLy";
             this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
             this.Text = "Quản lý";
